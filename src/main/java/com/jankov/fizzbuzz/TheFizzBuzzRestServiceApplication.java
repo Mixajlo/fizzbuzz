426f7058e5cbd83e7ae2cc2ed7b6c129825af9a5
Miki Jankov <miki.jankov87@gmail.com>
2021-09-01 11:32:48 +0200

    Initial commit

---
+++ src/main/java/com/jankov/fizzbuzz/TheFizzBuzzRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jankov.fizzbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFizzBuzzRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheFizzBuzzRestServiceApplication.class, args);
+	}
+
+}
