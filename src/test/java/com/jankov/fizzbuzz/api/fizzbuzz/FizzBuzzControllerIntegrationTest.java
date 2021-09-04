7966163a5c9ed8c1abe8553f5331e87622999614
Miki Jankov <miki.jankov87@gmail.com>
2021-09-04 13:54:33 +0200

    Implement integration tests

--- src/test/java/com/jankov/fizzbuzz/api/fizzbuzz/FizzBuzzControllerIntegrationTest.java
+++ src/test/java/com/jankov/fizzbuzz/api/fizzbuzz/FizzBuzzControllerIntegrationTest.java
@@ -18,9 +18,51 @@ class FizzBuzzControllerIntegrationTest {
     private int port;
 
     @Test
-    public void greetingShouldReturnDefaultMessage() {
+    public void ifEntryIsMultipleOf3ReturnFizz() {
+        var entry = 3;
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/fizzbuzz?entry=" + entry,
+                String.class)).contains("data\":\"fizz\"");
+    }
+
+    @Test
+    public void ifEntryIsMultipleOf5ReturnBuzz() {
         var entry = 5;
         assertThat(restTemplate.getForObject("http://localhost:" + port + "/fizzbuzz?entry=" + entry,
-                String.class)).contains("data\":\"" + entry);
+                String.class)).contains("data\":\"buzz\"");
+    }
+
+    @Test
+    public void ifEntryIsMultipleOf7ReturnBazz() {
+        var entry = 7;
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/fizzbuzz?entry=" + entry,
+                String.class)).contains("data\":\"Bazz\"");
+    }
+
+    @Test
+    public void ifEntryIsMultipleOf3And5ReturnFizzBuzz() {
+        var entry = 3 * 5;
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/fizzbuzz?entry=" + entry,
+                String.class)).contains("data\":\"fizzbuzz\"");
+    }
+
+    @Test
+    public void ifEntryIsMultipleOf3And7ReturnFizzBazz() {
+        var entry = 3 * 7;
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/fizzbuzz?entry=" + entry,
+                String.class)).contains("data\":\"FizzBazz\"");
+    }
+
+    @Test
+    public void ifEntryIsMultipleOf5And7ReturnBuzzBazz() {
+        var entry = 5 * 7;
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/fizzbuzz?entry=" + entry,
+                String.class)).contains("data\":\"BuzzBazz\"");
+    }
+
+    @Test
+    public void ifEntryIsMultipleOf3And5And7ReturnFizzBuzzBazz() {
+        var entry = 3 * 5 * 7;
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/fizzbuzz?entry=" + entry,
+                String.class)).contains("data\":\"FizzBuzzBazz\"");
     }
 }
