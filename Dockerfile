d4cb539df70c71f9fa9e6ba0b22c66a2f91875cb
Miki Jankov <miki.jankov87@gmail.com>
2021-09-04 15:07:52 +0200

    Run tests even on container Removed non used import

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ EXPOSE 8081
 
 RUN mkdir -p /usr/local/src/fizzbuzz
 ADD . /usr/local/src/fizzbuzz
-RUN cd /usr/local/src/fizzbuzz && ./mvnw clean package -DskipTests
+RUN cd /usr/local/src/fizzbuzz && ./mvnw clean package
 RUN mv /usr/local/src/fizzbuzz/target/*.jar /fizzbuzz.jar
 RUN rm -rf /usr/local/src/fizzbuzz
 CMD java -jar /fizzbuzz.jar
--- src/test/java/com/jankov/fizzbuzz/api/fizzbuzz/FizzBuzzControllerIntegrationTest.java
+++ src/test/java/com/jankov/fizzbuzz/api/fizzbuzz/FizzBuzzControllerIntegrationTest.java
@@ -6,7 +6,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
